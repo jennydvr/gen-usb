b18dffe5cf568e47b0356984383ed7326c4b4591
Jenny Valdez <jedaniv@gmail.com>
2013-03-05 14:26:24 -0430

    Primer commit

--- gen-usb/main.cpp
+++ gen-usb/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  gen-usb
 //
-//  Created by Jenny Valdez on 05/03/13.
+//  Created by Jenny Valdez & Luis Vieira on 05/03/13.
 //  Copyright (c) 2013 Jenny Valdez & Luis Vieira. All rights reserved.
 //
 
