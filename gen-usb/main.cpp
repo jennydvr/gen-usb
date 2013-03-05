e2bb743084c01963d248f803a7921a050b7f95e3
Jenny Valdez <jedaniv@gmail.com>
2013-03-05 14:25:07 -0430

    Initial Commit

---
+++ gen-usb/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gen-usb
+//
+//  Created by Jenny Valdez on 05/03/13.
+//  Copyright (c) 2013 Jenny Valdez & Luis Vieira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
